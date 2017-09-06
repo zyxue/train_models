195002bd7b459d3abeaeeb6eb6d5283109f0e755
Zhuyi Xue <alfred532008@gmail.com>
2017-09-06 07:51:10 -0700

    added logging to run_cv.py

--- run_cv.py
+++ run_cv.py
@@ -1,9 +1,13 @@
 import multiprocessing
+import logging
 
 import pandas as pd
 import numpy as np
 
 import cv
+
+logging.basicConfig(
+    level=logging.DEBUG, format='%(asctime)s|%(levelname)s|%(message)s')
 
 
 LABEL_GROUPS = [
@@ -13,6 +17,7 @@ LABEL_GROUPS = [
     ['UCEC-T', 'UCS-T'],
     ['ESCA-T', 'STAD-T'],
     ['PRAD-N', 'PRAD-T'],
+    ['COAD-T', 'READ-T'],
 
     ['KICH-N', 'KIRC-N', 'KIRP-N'],
     ['KICH-T', 'KIRC-T', 'KIRP-T']
@@ -28,15 +33,23 @@ for lg in LABEL_GROUPS:
     df_trva, df_test = cv.filter_by_label_group(DF_TRVA, DF_TEST, lg)
     df_trva, df_test = cv.preprocess(df_trva, df_test, lg)
 
-    Cs = 10 ** np.linspace(-2, 4, 50)
+    Cs = 10 ** np.arange(-2, 4, 0.1)
+    logging.info('working on {0} C values'.format(len(Cs)))
     cv_args = cv.gen_cv_args(df_trva, Cs)
-    with multiprocessing.Pool(50) as p:
+    with multiprocessing.Pool(60) as p:
         cv_res = p.map(cv.cv_fit_wrapper, cv_args)
 
     df_cv = pd.DataFrame(
         cv_res, columns=['c_val', 'logloss', 'auroc', 'num_genes'])
 
+    df_cv.to_csv(
+        './second-stage-models/{0}-cv.csv'.format('-'.join(lg)), index=False)
+
     with multiprocessing.Pool(50) as p:
-        num_genes = p.map(cv.calc_cv_num_genes_wrapper,
+        num_genes_trva = p.map(cv.calc_cv_num_genes_wrapper,
                           [(df_trva, _) for _ in Cs])
-    df_cv.to_csv('./{0}-cv.csv'.format('-'.join(lg)), index=False)
+
+    df_num_genes_trva = pd.DataFrame(
+        np.array([Cs, num_genes_trva]).T, columns=['c_val', 'num_genes'])
+    df_num_genes_trva.to_csv(
+        './second-stage-models/{0}-num-genes-trva.csv'.format('-'.join(lg)), index=False)
