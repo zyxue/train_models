bda806bc1b7a91a873fff6a3cbcbb961d838552a
Zhuyi Xue <alfred532008@gmail.com>
2017-09-07 08:46:47 -0700

    debugged run_cv_master_models.py

--- run_cv_master_models.py
+++ run_cv_master_models.py
@@ -29,9 +29,9 @@ elif master_model_name == 'all-with-class-merge':
     df_test = DF_TEST
     df_trva['label'] = df_trva.apply(cv.create_label_with_merging, axis=1)
     df_test['label'] = df_test.apply(cv.create_label_with_merging, axis=1)
-    
+
 elif master_model_name == "nt":
-    diseases_with_sufficient_normal = [
+    dises_with_suff_normal = [
         'BRCA', 'COAD', 'KICH',
         'KIRC', 'KIRP', 'LIHC',
         'LUAD', 'LUSC', 'THCA',
@@ -39,8 +39,10 @@ elif master_model_name == "nt":
         'BLCA', 'PRAD'
     ]
 
-    df_trva = DF_TRVA.query('disease in {0}'.format(diseases_with_sufficient_normal))
-    df_test = DF_TEST.query('disease in {0}'.format(diseases_with_sufficient_normal))
+    df_trva = DF_TRVA.query(
+        'disease in {0}'.format(dises_with_suff_normal)).copy()
+    df_test = DF_TEST.query(
+        'disease in {0}'.format(dises_with_suff_normal)).copy()
 
 elif master_model_name == 'all-t':
     lg = [_ for _ in DF_TRVA.label.unique() if _.endswith('-T')]
@@ -49,8 +51,11 @@ elif master_model_name == 'all-t':
     # merge hard-to-classify classes
     df_trva['label'] = df_trva.apply(cv.create_label_with_merging, axis=1)
     df_test['label'] = df_test.apply(cv.create_label_with_merging, axis=1)
-    lg = df_trva.label.unique()
-    df_trva, df_test = cv.preprocess(df_trva, df_test, lg)
+
+
+logging.info('preprocessing...')
+lg = df_trva.label.unique()
+df_trva, df_test = cv.preprocess(df_trva, df_test, lg)
 
 
 Cs = 10 ** np.arange(-2, 2, 0.1)
@@ -59,17 +64,17 @@ logging.info('working on {0} C values'.format(len(Cs)))
 logging.info('start grid search')
 
 grid_search_result = cv.cv_master(df_trva, Cs)
-df_cv = cv.gen_df_cv_master(grid_search_result)
+df_cv = cv.generate_df_cv_master(grid_search_result)
 df_cv.to_csv(
-    './master-models/{0}-cv.csv'.format('-'.join(master_model_name)), index=False)
+    './master-models/{0}-cv.csv'.format(master_model_name), index=False)
 
 
-logging.info('refit to obtain gene number counts')
-with multiprocessing.Pool(40) as p:
-    num_genes_trva = p.map(cv.calc_cv_num_genes_wrapper,
-                           [(df_trva, _) for _ in Cs])
+# logging.info('refit to obtain gene number counts')
+# with multiprocessing.Pool(40) as p:
+#     num_genes_trva = p.map(cv.calc_cv_num_genes_wrapper,
+#                            [(df_trva, _) for _ in Cs])
 
-df_num_genes_trva = pd.DataFrame(
-    np.array([Cs, num_genes_trva]).T, columns=['c_val', 'num_genes'])
-df_num_genes_trva.to_csv(
-    './master-models-models/{0}-num-genes-trva.csv'.format('-'.join(master_model_name)), index=False)
+# df_num_genes_trva = pd.DataFrame(
+#     np.array([Cs, num_genes_trva]).T, columns=['c_val', 'num_genes'])
+# df_num_genes_trva.to_csv(
+#     './master-models-models/{0}-num-genes-trva.csv'.format('-'.join(master_model_name)), index=False)
